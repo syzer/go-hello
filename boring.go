18ea43aa0f18ce03aef35e621d7f563d3b6ff0af
syzer3@gmail.com <syzer3@gmail.com>
2016-08-30 09:15:35 +0200

    adding range

---
+++ boring.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"time"
+	"math/rand"
+)
+
+func main() {
+	c := make(chan string)
+	fmt.Printf("Hello world!")
+	fmt.Println("Listening")
+	// couroutine
+	go boring("boring ", c)
+
+	for i := 0; i < 10; i++ {
+		// receive expression is just a value
+		fmt.Printf("You say: %q\n", <-c)
+	}
+	time.Sleep(10 * time.Second)
+	fmt.Println("Timeout bitches")
+}
+
+func boring(msg string, c chan string) {
+	for i := 0; ; i++ {
+		c <- fmt.Sprintf("%s %d", msg, i)
+		//		fmt.Println(msg, i)
+		time.Sleep(time.Duration(rand.Intn(1000)) * time.Millisecond)
+	}
+}
+
