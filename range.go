0929f1ca12d8ef46cdd97c9ba231af3ee1e21a78
syzer3@gmail.com <syzer3@gmail.com>
2016-08-30 09:09:17 +0200

    adding range

--- range.go
+++ range.go
@@ -5,14 +5,20 @@ import "fmt"
 var pow = []int{1, 2, 4, 8, 16, 32, 64, 128}
 
 func main() {
+	// le normal
 	for i, v := range pow {
 		fmt.Printf("2**%d = %d\n", i, v)
 	}
 
 	pow := make([]int, 10)
+
+	// just a index
 	for i := range pow {
+		// shift
 		pow[i] = 1 << uint(i) // == 2**i
 	}
+
+	// just a value
 	for _, value := range pow {
 		fmt.Printf("%d\n", value)
 	}
