4793101e5f1fc46572b44c75976c9757f0655745
syzer3@gmail.com <syzer3@gmail.com>
2016-09-04 10:10:37 +0200

    recover from panic

---
+++ panic/panicRecover.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+// 0
+// <nil>
+// 3
+func main() {
+	fmt.Println(divide(1, 0))
+	fmt.Println(divide(6, 2))
+
+	withPanic()
+
+
+	x :=0
+	changeValue(&x)
+	fmt.Println("x is:", x)
+}
+
+func divide(a, b int) int {
+	defer func() {
+		fmt.Println(recover())
+	}()
+	return a / b
+}
+
+// PANIC
+func withPanic() {
+	defer func() {
+		fmt.Println(recover())
+	}()
+	panic("PANIC")
+}
+
+func changeValue(x *int) {
+	*x = 666
+}
