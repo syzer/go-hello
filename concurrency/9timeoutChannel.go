c11251e5224308f13cd4024500797fcb26833389
syzer3@gmail.com <syzer3@gmail.com>
2016-09-08 21:49:08 +0200

    timeout channel

---
+++ concurrency/9timeoutChannel.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"math/rand"
+	"time"
+)
+
+func main() {
+	// like stream.join
+	c := generator4("dupa")
+	timeout := time.After(3 * time.Second)
+
+	for {
+		select {
+		case s := <-c:
+			log.Println(s)
+		case <-timeout:
+			log.Println("Done")
+			return
+		}
+	}
+}
+
+// func that returns channel
+// returned channel is receive only
+func generator4(msg string) <-chan string {
+	c := make(chan string)
+	go func() {
+		for i := 0; i < 5; i++ {
+			c <- fmt.Sprintf("calling %s %d", msg, i)
+			time.Sleep(time.Duration(rand.Intn(500)) * time.Millisecond)
+		}
+	}()
+	return c
+}
