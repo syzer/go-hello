8c07ed0d43844018c91d5b87fec5065fbff93f6c
syzer3@gmail.com <syzer3@gmail.com>
2016-09-08 10:19:36 +0200

    channel pass expression

---
+++ concurrency/3channel.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"log"
+	"math/rand"
+	"time"
+	"fmt"
+)
+
+func main() {
+	c := make(chan string)
+
+	go callMeMaybe3("dupa", c)
+
+	for i := 0; i < 5; i++ {
+		log.Printf("channel emmited msg: %v", <-c)
+	}
+	log.Println("Done")
+}
+
+func callMeMaybe3(msg string, c chan string) {
+	for i := 0; i < 5; i++ {
+		// pass expression!!
+		c <- fmt.Sprintf("calling %s %d", msg, i)
+		time.Sleep(time.Duration(rand.Intn(500)) * time.Millisecond)
+	}
+}
